8673846e341fea6d38bc64efd74b3fad6ff4f826
tomoya0220-gz <tomoya02201998@gmail.com>
2024-04-30 22:01:55 +0900

    軽微な修正

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -16,24 +16,20 @@ export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/signin" element={<SignIn />} />
-        <Route exact path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
         {auth ? (
           <>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/task/new" element={<NewTask />} />
-            <Route exact path="/list/new" element={<NewList />} />
-            <Route
-              exact
-              path="/lists/:listId/tasks/:taskId"
-              element={<EditTask />}
-            />
-            <Route exact path="/lists/:listId/edit" element={<EditList />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/task/new" element={<NewTask />} />
+            <Route path="/list/new" element={<NewList />} />
+            <Route path="/lists/:listId/tasks/:taskId" element={<EditTask />} />
+            <Route path="/lists/:listId/edit" element={<EditList />} />
           </>
         ) : (
-          <Route path="/signin" element={<Navigate to="/signin" replace />} />
+          <Route path="*" element={<Navigate to="/signin" replace />} />
         )}
-        <Route element={NotFound} />
+        <Route path="*" element={NotFound} />
       </Routes>
     </BrowserRouter>
   );
